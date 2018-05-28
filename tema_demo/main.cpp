cf0bcc25415ec8b4979896af9bfe35dccee24bf0
Oana Vasut <oana@mushu.localdomain>
2018-05-28 13:36:09 +0300

    primul test la tema demo

---
+++ tema_demo/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * short, long int - intreg, 5
+ * double - real, 3.45
+ * float - real 3.65
+ * char - caracter 'g'
+ *
+ * 00 00 00 00 - 0
+ * 00 10 00 01 - !
+ */
+
+
+int main(int  argc, char *argv[]){
+
+}
